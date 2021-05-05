19d820e00d3b48d45293463a77edd4b54d69c54d
le Téno <leteno.loic@gmail.com>
2021-05-05 12:23:30 +0200

    fin exo (correction) LLT

---
+++ main.js
@@ -0,0 +1,14 @@
+// ## EXO 1
+let array = ["Farhad", "Yassin", "Alexis", "Kevin", "Junior", "Andy", "Tania", "Issam", "Mohammed","Mihai", "Loïc", "Charles", "William", "Oussama", "Ilyas", "Anthony", "Dorian", "Alain"];
+
+array.forEach(i => {
+    console.log(`Bonjour, ${i}`);
+});
+
+
+// ## EXO 2
+let array2 = ["Guitare", 69, true, 75, "lolipop"];
+
+array2.forEach(i => {
+    console.log(i);
+});
